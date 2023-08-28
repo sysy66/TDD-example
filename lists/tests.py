6ae9d2e044d8f9cad401e50860e911c26e512f30
caolv <shaoyinshaoyang@gmail.com>
2023-08-28 20:12:12 +0800

    delete useless packages

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,4 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
-from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
