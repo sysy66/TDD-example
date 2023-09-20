71451c02bfd2d5569a18dd77f7a14da8e7f4badf
caolv <shaoyinshaoyang@gmail.com>
2023-09-20 21:28:43 +0800

    Add a URL for creating a new list via POST

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import lists.views as views
+from lists import views
 
 # path('admin/', admin.site.urls),
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
+    path('lists/new', views.new_list, name='new_list'),
 ]
