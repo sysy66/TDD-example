67f23ec3eab1e7af95f891b24eeae8e842c01972
caolv <shaoyinshaoyang@gmail.com>
2023-09-21 20:35:41 +0800

    7.12.4

--- superlists/urls.py
+++ superlists/urls.py
@@ -21,6 +21,7 @@ from lists import views
 # path('admin/', admin.site.urls),
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
     path('lists/new', views.new_list, name='new_list'),
+    path('lists/<int:list_id>/', views.view_list, name='view_list'),
+    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
 ]
