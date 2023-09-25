9fed8726c8c0a7c28d68169c11e001c1b4327bc8
caolv <shaoyinshaoyang@gmail.com>
2023-09-25 20:41:41 +0800

    move sqlite and static outside of main source tree

--- superlists/settings.py
+++ superlists/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+# BASE.DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -77,7 +79,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "../database/db.sqlite3"),
     }
 }
 
@@ -118,8 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-import os
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "./static"))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "../static"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
